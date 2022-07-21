4900ca58cc9f3643f0f14c26b42bb526460a3d25
kevinosbornjb <kevinosborn31@gmail.com>
2022-07-21 22:14:11 +1000

    Cleaned up

--- compile.js
+++ compile.js
@@ -2,7 +2,9 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
+const contractCount = 1;
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
+const compiledContracts = solc.compile(source, contractCount).contracts;
 
-solc.compile(source, 1);
+module.exports = compiledContracts[':Inbox'];
