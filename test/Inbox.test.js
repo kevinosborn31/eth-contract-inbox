442b6295cafdc708f8e2822c77a26d5c1a8e5fc9
kevinosbornjb <kevinosborn31@gmail.com>
2022-07-22 09:05:57 +1000

    Contract deployment in mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,23 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const Web3Constructor = require('web3');
+const web3 = new Web3Constructor(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-// Create new web3 instance with ganache
-const web3 = new Web3(ganache.provider());
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox);
+    });
+});
